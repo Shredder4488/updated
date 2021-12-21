2a92be1a8d2d6f74d1022650617771437390c02c
Anchit Agarwal <65672924+Shredder4488@users.noreply.github.com>
2021-12-21 13:09:35 +0530

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -9,3 +9,5 @@
  if (n > x.length) {slideIndex = 1} if (n < 1) {slideIndex = x.length}for
  (i = 0; i < x.length; i++) {x[i].style.display = "none";}
  x[slideIndex-1].style.display = "block"; }
+
+ 
