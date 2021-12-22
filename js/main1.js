36ccf0556490a2a939aae8d2b4eda290d1953f12
DezyNation <87603422+DezyNation@users.noreply.github.com>
2021-12-23 03:34:58 +0530

    Add files via upload

--- js/main1.js
+++ js/main1.js
@@ -1,32 +1,167 @@
-var slideIndex = 1;
-var slide = document.getElementsByClassName("mySlides")
-var counterVal = 0;
-var prevSlide = document.getElementById("previous")
-var currentSlide = document.getElementById("current")
-var nextSlide = document.getElementById("next")
+var counterRight = 0;
+var counterLeft = 0;
+var newImage = document.createElement("img");
+var slide1 = document.getElementById("slide1");
+var slide2 = document.getElementById("slide2");
+var slide3 = document.getElementById("slide3");
 
-const imageArr = ["slide1.jpg","slide2.jpg","slide3.jpg","slide4.jpg","slide5.jpg"];
-const childImages = document.querySelector("#wrapper").childNodes;
-var tempImg = "temporary address";
+const images = document.querySelector("#wrapper")
 
-document.getElementById("leftbtn").addEventListener("click", () => {
-  if(counterVal>1){
-  counterVal = counterVal - 1;
-  TweenLite.to(slide, 1, {x:-counterVal*300});
-  }
-  else
-  {
-    counterVal = 0;
-    TweenLite.to(slide, 1, {x:0});
-  }
-})
 
 document.getElementById("rightbtn").addEventListener("click", () => {
-  counterVal = counterVal + 1;
-  TweenLite.to(slide, 1, {x:counterVal*-300});
-  for(i=0; i<=childImages.length; i++){
-    tempImg = childImages[0];
-    childImages[i-1] = childImages[i];
-    childImages[childImages.length] = tempImg;
+  ++counterLeft;
+  if(counterRight%3 == 0 && counterLeft%3 == 1){
+    slide3.style.zIndex = 5;
+    slide2.style.zIndex = 0;
+    slide1.style.zIndex = 0;
+    TweenLite.to(slide1, 1, {x: 600});
+    TweenLite.to(slide2, 1, {x: -300});
+    TweenLite.to(slide3, 1, {x: -300});
   }
+  else if(counterRight%3 == 1 && counterLeft%3 == 2){
+    slide3.style.zIndex = 5;
+    slide2.style.zIndex = 0;
+    slide1.style.zIndex = 0;
+    TweenLite.to(slide1, 1, {x: 600});
+    TweenLite.to(slide2, 1, {x: -300});
+    TweenLite.to(slide3, 1, {x: -300});
+  }
+  else if(counterRight%3 == 2 && counterLeft%3 == 0){
+    slide3.style.zIndex = 5;
+    slide2.style.zIndex = 0;
+    slide1.style.zIndex = 0;
+    TweenLite.to(slide1, 1, {x: 600});
+    TweenLite.to(slide2, 1, {x: -300});
+    TweenLite.to(slide3, 1, {x: -300});
+  }
+
+  //Sequence 2
+  else if(counterRight%3 == 0 && counterLeft%3 == 2){
+    slide1.style.zIndex = 5;
+    slide3.style.zIndex = 0;
+    slide2.style.zIndex = 0;
+    TweenLite.to(slide1, 1, {x: 300});
+    TweenLite.to(slide2, 1, {x: 300});
+    TweenLite.to(slide3, 1, {x: -600});
+  }
+  else if(counterRight%3 == 2 && counterLeft%3 == 1){
+    slide1.style.zIndex = 5;
+    slide3.style.zIndex = 0;
+    slide2.style.zIndex = 0;
+    TweenLite.to(slide1, 1, {x: 300});
+    TweenLite.to(slide2, 1, {x: 300});
+    TweenLite.to(slide3, 1, {x: -600});
+  }
+
+  //Reset
+  else if(counterRight%3 == 0 && counterLeft%3 == 0){
+    slide2.style.zIndex = 5;
+    slide1.style.zIndex = 0;
+    slide3.style.zIndex = -1;
+    TweenLite.to(slide1, 1, {x: 0});
+    TweenLite.to(slide2, 1, {x: 0});
+    TweenLite.to(slide3, 1, {x: 0});
+  }
+  else if (counterRight%3 == 1 && counterLeft%3 == 1){
+    slide2.style.zIndex = 5;
+    slide1.style.zIndex = 0;
+    slide3.style.zIndex = -1;
+    TweenLite.to(slide1, 1, {x: 0});
+    TweenLite.to(slide2, 1, {x: 0});
+    TweenLite.to(slide3, 1, {x: 0});
+  }
+  else if (counterRight%3 == 2 && counterLeft%3 == 2){
+    slide2.style.zIndex = 5;
+    slide1.style.zIndex = 0;
+    slide3.style.zIndex = -1;
+    TweenLite.to(slide1, 1, {x: 0});
+    TweenLite.to(slide2, 1, {x: 0});
+    TweenLite.to(slide3, 1, {x: 0});
+  }
+  console.log("Right changed to = "+counterRight%3);
+  console.log("Left changed to = "+counterLeft%3);
 })
+
+
+document.getElementById("leftbtn").addEventListener("click", () => {
+  ++counterRight;
+  if(counterRight%3 == 1 && counterLeft%3 == 0){
+    slide1.style.zIndex = 5;
+    slide3.style.zIndex = -1;
+    slide2.style.zIndex = 0;
+    TweenLite.to(slide1, 1, {x: 300});
+    TweenLite.to(slide2, 1, {x: 300});
+    TweenLite.to(slide3, 1, {x: -600});
+  }
+  else if(counterRight%3 == 2 && counterLeft%3 == 1){
+    slide1.style.zIndex = 5;
+    slide3.style.zIndex = -1;
+    slide2.style.zIndex = 0;
+    TweenLite.to(slide1, 1, {x: 300});
+    TweenLite.to(slide2, 1, {x: 300});
+    TweenLite.to(slide3, 1, {x: -600});
+  }
+  else if(counterRight%3 == 0 && counterLeft%3 == 2){
+    slide1.style.zIndex = 5;
+    slide3.style.zIndex = -1;
+    slide2.style.zIndex = 0;
+    TweenLite.to(slide1, 1, {x: 300});
+    TweenLite.to(slide2, 1, {x: 300});
+    TweenLite.to(slide3, 1, {x: -600});
+  }
+
+  //Sequence 2
+  else if(counterRight%3 == 2 && counterLeft%3 == 0){
+    slide3.style.zIndex = 5;
+    slide2.style.zIndex = -1;
+    slide1.style.zIndex = 0;
+    TweenLite.to(slide1, 1, {x: 600});
+    TweenLite.to(slide2, 1, {x: -300});
+    TweenLite.to(slide3, 1, {x: -300});
+  }
+  else if(counterRight%3 == 1 && counterLeft%3 == 2){
+    slide3.style.zIndex = 5;
+    slide2.style.zIndex = -1;
+    slide1.style.zIndex = 0;
+    TweenLite.to(slide1, 1, {x: 600});
+    TweenLite.to(slide2, 1, {x: -300});
+    TweenLite.to(slide3, 1, {x: -300});
+  }
+  else if(counterRight%3 == 0 && counterLeft%3 == 1){
+    slide3.style.zIndex = 5;
+    slide2.style.zIndex = -1;
+    slide1.style.zIndex = 0;
+    TweenLite.to(slide1, 1, {x: 600});
+    TweenLite.to(slide2, 1, {x: -300});
+    TweenLite.to(slide3, 1, {x: -300});
+  }
+
+  //Reset
+  else if(counterRight%3 == 1 && counterLeft%3 == 1){
+    slide2.style.zIndex = 5;
+    slide1.style.zIndex = -1;
+    slide3.style.zIndex = 0;
+    TweenLite.to(slide1, 1, {x: 0});
+    TweenLite.to(slide2, 1, {x: 0});
+    TweenLite.to(slide3, 1, {x: 0});
+  }
+  else if(counterRight%3 == 2 && counterLeft%3 == 2){
+    slide2.style.zIndex = 5;
+    slide1.style.zIndex = -1;
+    slide3.style.zIndex = 0;
+    TweenLite.to(slide1, 1, {x: 0});
+    TweenLite.to(slide2, 1, {x: 0});
+    TweenLite.to(slide3, 1, {x: 0});
+  }
+  else if(counterRight%3 == 0 && counterLeft%3 == 0){
+    slide2.style.zIndex = 5;
+    slide1.style.zIndex = -1;
+    slide3.style.zIndex = 0;
+    TweenLite.to(slide1, 1, {x: 0});
+    TweenLite.to(slide2, 1, {x: 0});
+    TweenLite.to(slide3, 1, {x: 0});
+  }
+  console.log("Right changed to = "+counterRight%3);
+  console.log("Left changed to = "+counterLeft%3);
+})
+
--- js/main2.js
+++ js/main2.js
@@ -1,119 +1,38 @@
-console.clear();
+var scroller = document.getElementById("scroller");
+var slides = document.getElementsByClassName("mySlides")
+var x = -600;
+var i = 4;
+var children = document.getElementById("scroller").childNodes;
+var imageArray = ["img1/slide1.jpg", "img1/slide2.jpg", "img1/slide3.jpg", "img1/slide4.jpg", "img1/slide5.jpg"];
 
-var slideDelay = 1.5;
-var slideDuration = 0.3;
 
-var slides = document.querySelectorAll(".mySlides");
-var prevButton = document.querySelector("#leftbtn");
-var nextButton = document.querySelector("#rightbtn");
-
-var numSlides = slides.length;
-
-for (var i = 0; i < numSlides; i++) {
-  TweenLite.set(slides[i], {
-    backgroundColor: Math.random() * 0xffffff,
-    xPercent: i * 100
-  });
-}
-
-var wrap = wrapPartial(-100, (numSlides - 1) * 100);
-var timer = TweenLite.delayedCall(slideDelay, autoPlay);
-
-var animation = TweenMax.to(slides, 1, {
-  xPercent: "+=" + (numSlides * 100),
-  ease: Linear.easeNone,
-  paused: true,
-  repeat: -1,
-  modifiers: {
-    xPercent: wrap
-  }
-});
-
-var proxy = document.createElement("div");
-TweenLite.set(proxy, { x: "+=0" });
-var transform = proxy._gsTransform;
-var slideAnimation = TweenLite.to({}, 0.1, {});
-var slideWidth = 0;
-var wrapWidth = 0;
-resize();
-
-var draggable = new Draggable(proxy, {
-  trigger: ".slides-container",
-  throwProps: true,
-  onPress: updateDraggable,
-  onDrag: updateProgress,
-  onThrowUpdate: updateProgress,
-  snap: {     
-    x: snapX
-  }
-});
-
-window.addEventListener("resize", resize);
-
-prevButton.addEventListener("click", function() {
-  animateSlides(1);
-});
-
-nextButton.addEventListener("click", function() {
-  animateSlides(-1);
-});
-
-function updateDraggable() {
-  
-  timer.restart(true);
-  slideAnimation.kill();
-  this.update();
-}
-
-function animateSlides(direction) {
-    
-  timer.restart(true);
-  slideAnimation.kill();
-  
-  var x = snapX(transform.x + direction * slideWidth);
-  
-  slideAnimation = TweenLite.to(proxy, slideDuration, {
-    x: x,
-    onUpdate: updateProgress
-  });  
-}
-
-function autoPlay() {  
-    
-  if (draggable.isPressed || draggable.isDragging || draggable.isThrowing) {
-    timer.restart(true);
-  } else {
-    animateSlides(-1);
+function createImgForward(){
+  for(var j=0; j<=4; j++){
+    var newImage = document.createElement("img");
+    newImage.setAttribute("class", "mySlides");
+    newImage.setAttribute("src", imageArray[j]);
+    scroller.appendChild(newImage);
   }
 }
 
-function updateProgress() {  
-  animation.progress(transform.x / wrapWidth);
+
+function createImgBackward(){
+    // var first = document.getElementById("scroller").firstElementChild;
+    // var last = document.getElementById("scroller").lastChild;
+    var newImage = document.createElement("img");
+    newImage.setAttribute("class", "mySlides");
+    newImage.setAttribute("src", imageArray[1]);
+    document.getElementById("scroller").insertBefore(newImage, children[0]);
 }
 
-function snapX(x) {
-  return Math.round(x / slideWidth) * slideWidth;
-}
 
-function resize() {
-  
-  var norm = (transform.x / wrapWidth) || 0;
-  
-  slideWidth = slides[0].offsetWidth;
-  wrapWidth = slideWidth * numSlides;
-  
-  TweenLite.set(proxy, {
-    x: norm * wrapWidth
-  });
-  
-  animateSlides(0);
-  slideAnimation.progress(1);
-}
+document.getElementById("rightbtn").addEventListener("click", ()=> {
+  createImgForward()
+  TweenLite.to(slides, 1, {x: x-=300});
+})
 
-function wrapPartial(min, max) {  
-  var r = max - min;  
-  return function(value) {
-    var v = value - min;
-    return ((r + v % r) % r) + min;
-  }
-}
+
+document.getElementById("leftbtn").addEventListener("click", ()=> {
+  TweenLite.to(slides, 1, {x: x+=300});
+  imageArray.forEach(createImgBackward);
+})
